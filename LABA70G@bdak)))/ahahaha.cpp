2e02bf6c3ed1480ffb245cc0bceeca54be162e98
1NT3lCore <148251080+1NT3lCore@users.noreply.github.com>
2024-05-21 01:26:51 +0300

    Update ahahaha.cpp

--- LABA70G@bdak)))/ahahaha.cpp
+++ LABA70G@bdak)))/ahahaha.cpp
@@ -4,26 +4,32 @@ Node* createList() {
     return nullptr;
 }
 
+// Этот метод добавляет новый элемент со значением value в начало списка
 void addElement(Node*& head, int value) {
-    Node* newNode = new Node;
-    newNode->data = value;
-    newNode->next = head;
-    head = newNode;
+    Node* newNode = new Node; // Создание нового узла
+    newNode->data = value; // Присвоение узлу значения
+    newNode->next = head; // Установка следующего указателя на текущий head
+    head = newNode; // head теперь указывает на новый узел
 }
 
+// Этот метод удаляет элемент со значением value из списка
 void deleteElement(Node*& head, int value) {
-    Node* current = head;
-    Node* previous = nullptr;
+    Node* current = head; // Текущий указатель на начало списка
+    Node* previous = nullptr; // Предыдущий указатель, инициализирован как nullptr
+
+        // Поиск элемента со значением value в списке
         while (current != nullptr && current->data != value) {
             previous = current;
             current = current->next;
         }
 
+    // Если элемент не найден
     if (current == nullptr) {
         std::cout << "Element not found in the list." << std::endl;
         return;
     }
 
+    // Удаление найденного элемента
     if (previous != nullptr) {
         previous->next = current->next;
     }
@@ -31,19 +37,20 @@ void deleteElement(Node*& head, int value) {
         head = current->next;
     }
 
-    delete current;
+    delete current; // Освобождение памяти
 }
 
+// Этот метод меняет местами минимальный и максимальный элементы списка
 void swapMinMax(Node* head) {
     if (head == nullptr) {
         std::cout << "List is empty." << std::endl;
         return;
     }
+        Node* minNode = head; // Узел с минимальным значением, инициализирован как первый элемент
+    Node* maxNode = head; // Узел с максимальным значением, инициализирован как первый элемент
+    Node* current = head; // Текущий элемент, инициализирован как первый элемент
 
-        Node* minNode = head;
-    Node* maxNode = head;
-    Node* current = head;
-
+    // Поиск минимального и максимального элементов в списке
     while (current != nullptr) {
         if (current->data < minNode->data) {
             minNode = current;
@@ -56,6 +63,7 @@ void swapMinMax(Node* head) {
         current = current->next;
     }
 
+    // Обмен значениями минимального и максимального элементов
     int temp = minNode->data;
     minNode->data = maxNode->data;
     maxNode->data = temp;
